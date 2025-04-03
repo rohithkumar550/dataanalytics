50b6ab864d235ac5bc4821fddc76c27a5a8fbf97
Rohith Kumar <135095391+rohithkumar550@users.noreply.github.com>
2025-04-03 20:19:47 +0530

    Add files via upload

---
+++ nestedif.py
@@ -0,0 +1,12 @@
+# Program to find the grade of a student based on marks
+# Input marks from user
+marks = int(input("Enter the marks: "))
+if marks<0 or marks>100:
+ if marks>=90 and marks<=100:
+    print("Grade A")
+elif marks>=70 and marks<=80:
+    print("Grade B")
+elif marks>=60 and marks<=70:
+    print("Grade C")
+else:
+    print("fail")
